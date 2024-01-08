c7a368c406a9feb6183dfb0d62b1029eafed1f57
Sergey <sergeykaynoy@gmail.com>
2024-01-08 20:05:56 +0700

    Work with TreedNode

--- src/main.py
+++ src/main.py
@@ -1,11 +1,32 @@
-import requests
-import time
-import aiohttp
+class TreedNode:
+    def __init__(self, key):
+        self.key = key
+        self.left = None
+        self.right = None
+
+    def __str__(self):
+        return f'key: {self.key}\n' \
+               f'left {self.left}\n' \
+               f'right {self.right}\n'
 
 
-def main():
-    pass
+def copy_tree(root: TreedNode):
+    if root is None:
+        return None
+
+    new_root = TreedNode(root.key)
+    new_root.left = copy_tree(root.left)
+    new_root.right = copy_tree(root.right)
+
+    return new_root
 
 
-if __name__ == '__main__':
-    pass
+def find_max_key(root: TreedNode):
+    if root is None:
+        return float('-inf')
+
+    max_key = root.key
+    max_left = find_max_key(root.left)
+    max_right = find_max_key(root.right)
+
+    return max(max_key, max_left, max_right)
